8e36c6438008b42f97ad70a42e1ae6f8d9570664
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-11-19 10:25:40 +0100

    HTMX BABYYYY

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,6 +13,7 @@ const port int = 8000
 type Job struct {
 	Id      string
 	Command string
+	Status  string
 }
 
 type Account struct {
@@ -20,19 +22,45 @@ type Account struct {
 
 var account = Account{
 	Jobs: []Job{
-		{Id: "1", Command: "echo 'Hello world'"},
+		{
+			Id:      "1",
+			Command: "echo 'Hello world'",
+			Status:  "success",
+		},
 	},
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	log.Print("Page index")
-	tmpl := template.Must(template.ParseFiles("index.htmx"))
-	tmpl.Execute(w, account)
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	requestLogJsonBytes, marshalError := json.Marshal(map[string]string{
+		"method": r.Method,
+		"path":   r.URL.Path,
+	})
+	if marshalError != nil {
+		log.Panicln(marshalError)
+	}
+	log.Println(string(requestLogJsonBytes))
+	tmpl := template.Must(template.ParseGlob("templates/*"))
+	tmpl.ExecuteTemplate(w, "index", account)
+}
+
+func HandleJobs(w http.ResponseWriter, r *http.Request) {
+	job := Job{
+		Id:      "id",
+		Command: "command",
+		Status:  "status",
+	}
+	tmpl := template.Must(template.ParseGlob("templates/*"))
+	tmpl.ExecuteTemplate(w, "job-row", job)
 }
 
 func main() {
-	// routes
-	http.HandleFunc("/", Index)
+	// static files
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// API routes
+	http.HandleFunc("/api/jobs", HandleJobs)
+	// HTML routes
+	http.HandleFunc("/", HandleIndex)
 
 	// start server
 	address := fmt.Sprintf(":%d", port)
